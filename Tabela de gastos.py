f45dd3f49c8a47000a580a1067de867e7d749b19
Cássio Estevão <116197477+cassioestevao@users.noreply.github.com>
2024-03-04 16:24:20 -0300

    Tabela de gastos.py

--- Tabela de gastos.py
+++ Tabela de gastos.py
@@ -1,40 +1,64 @@
 import tkinter as tk
-from openpyxl import Workbook
-
-def save_expense():
-    expense = expense_entry.get()
-    category = category_entry.get()
-    date = date_entry.get()
+from tkinter import messagebox
+import csv
+from datetime import datetime
 
 
-    #Def execel
-    wb = Workbook()
-    sheet = wb.active
-    sheet.append([expense, category, date])
-    wb.save("tab.xlsx")
+class CadastroNotaFiscal:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tabela de Gastos | Developer | Cássio Estevão")
+
+        self.label_numero = tk.Label(root, text="Descrição do produto :",padx=10,pady=10)
+        self.label_numero.pack()
+        self.entry_numero = tk.Entry(root)
+        self.entry_numero.pack()
+
+        self.label_data = tk.Label(root, text="Data da de compra do produto (dd/mm/aaaa):",padx=10,pady=10)
+        self.label_data.pack()
+        self.entry_data = tk.Entry(root)
+        self.entry_data.pack()
+
+        self.label_valor = tk.Label(root, text="Valor do produto:",padx=10,pady=10)
+        self.label_valor.pack()
+        self.entry_valor = tk.Entry(root)
+        self.entry_valor.pack()
+
+        self.button_cadastrar = tk.Button(root, text="Cadastrar", command=self.cadastrar,padx=10,pady=10)
+        self.button_cadastrar.pack()
+
+    def cadastrar(self):
+        numero = self.entry_numero.get()
+        data = self.entry_data.get()
+        valor = self.entry_valor.get()
+
+        if not numero or not data or not valor:
+            messagebox.showerror("Erro", "Por favor, preencha todos os campos.")
+            return
+
+        try:
+            datetime.strptime(data, "%d/%m/%Y")
+        except ValueError:
+            messagebox.showerror("Erro", "Formato de data inválido. Use dd/mm/aaaa.")
+            return
+
+        try:
+            valor = float(valor)
+        except ValueError:
+            messagebox.showerror("Erro", "Valor inválido. Use somente números.")
+            return
+
+        with open('Tabela de Gastos.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([numero, data, valor])
+        
+        messagebox.showinfo("Sucesso", "produto cadastrado com sucesso.")
+
+    def visualizar(self):
+        numero = self.entry_numero.get()
+        data = self.entry_data.get()
+        valor = self.entry_valor.get()
 
 root = tk.Tk()
-root.title("Controle de Gastos")
-
-expense_label = tk.Label(root, text="Gastos:")
-expense_label.grid(row=0, column=0)
-
-expense_entry = tk.Entry(root)
-expense_entry.grid(row=0, column=1, padx= 10)
-
-category_label = tk.Label(root, text="Descrição:")
-category_label.grid(row=1, column=0)
-
-category_entry = tk.Entry(root)
-category_entry.grid(row=1, column=1,padx=10, pady= 15)
-
-date_label = tk.Label(root, text="Data:")
-date_label.grid(row=2, column=0)
-
-date_entry = tk.Entry(root)
-date_entry.grid(row=2, column=1)
-
-save_button = tk.Button(root, text="Pull", command=save_expense)
-save_button.grid(row=3, column=1)
-
+app = CadastroNotaFiscal(root)
 root.mainloop()
